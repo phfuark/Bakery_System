3199870ecb6c1a016cf220743841d92056e16135
phfuark <paulohsilva2007@gmail.com>
2025-03-27 11:06:51 -0300

    Edit models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,22 +1,29 @@
 from django.db import models
 
-choicesEmployee = ['Attendant', 'Baker', 'Cleaner']
-
 class Employee(models.Model):
-    id_employee = models.PositiveIntegerField(primary_key=True)
-    function = models.TextChoices(choicesEmployee)
+    class FunctionChoices(models.TextChoices):
+        ATTENDANT = 'Attendant'
+        BAKER = 'Baker'
+        CLEANER = 'Cleaner'
 
-class Card(models.Model):
-    id_card = models.PositiveIntegerField(primary_key=True)
+    id_employee = models.AutoField(primary_key=True)
+    function = models.CharField(max_length=20, choices=FunctionChoices.choices)
 
 class Product(models.Model):
-    id_product = models.PositiveIntegerField(primary_key=True)    
-    name = models.CharField(max_length=100, primary_key=True)
+    id_product = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
+class Card(models.Model):
+    id_card = models.AutoField(primary_key=True)
+    product = models.ManyToManyField(Product)
+
 class Supplier(models.Model):
-    id_supplier = models.PositiveIntegerField(primary_key=True)
+    id_supplier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150)
-    adress = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=12)
 
+class Sale(models.Model):
+    id_sale = models.AutoField(primary_key=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="sales")
